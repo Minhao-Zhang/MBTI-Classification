642e26de2b42de7dd0369c1583e842e34c4081f3
Minhao Zhang <z.minhao.01+coding@gmail.com>
2024-07-22 22:13:29 +0800

    modified data due to cuda OOM, started on A100 server

--- preprocessing/combine_short_text.py
+++ preprocessing/combine_short_text.py
@@ -5,16 +5,16 @@ DATA_PATH = "../data/"
 data = pd.read_csv(f'{DATA_PATH}reddit_post.csv')
 
 # Function to concatenate body strings of each author
-def concatenate_bodies(group):
+def concatenate_bodies(group, min_length=500):
     concatenated = ''
     for body in group['body']:
-        if len(body) >= 300:
+        if len(body) >= min_length:
             yield body
         else:
             if len(concatenated) > 0:
                 concatenated += ' '
             concatenated += body
-            if len(concatenated) >= 300:
+            if len(concatenated) >= min_length:
                 yield concatenated
                 concatenated = ''
     if len(concatenated) > 0:
@@ -23,18 +23,28 @@ def concatenate_bodies(group):
 # List to collect the new rows
 new_rows = []
 
+n = 700
+
 # Group by 'author' and process each group
 for author, group in data.groupby('author'):
-    for concatenated_body in concatenate_bodies(group):
-        if len(concatenated_body) >= 300:
-            new_rows.append({
-                'author': author,
-                'body': concatenated_body,
-                'mbti': group.iloc[0]['mbti']  # assuming 'mbti' is the same for all rows of the same author
-            })
+    for concatenated_body in concatenate_bodies(group, n):
+        new_rows.append({
+            'author': author,
+            'body': concatenated_body,
+            'mbti': group.iloc[0]['mbti']  # assuming 'mbti' is the same for all rows of the same author
+        })
+        
 
 # Create a new DataFrame from the new rows
 new_df = pd.DataFrame(new_rows)
+
+# remove the rows with less than n char, print them 
+new_df = new_df[new_df['body'].str.len() >= n]
+# remove any value that are more than 90th percentile
+new_df = new_df[new_df['body'].str.len() <= new_df['body'].str.len().quantile(0.9)]
+
+# shuffle the data  
+new_df = new_df.sample(frac=1).reset_index(drop=True)
 
 # save it to a new CSV file
 new_df.to_csv(f'{DATA_PATH}reddit_post_combined.csv', index=False)
