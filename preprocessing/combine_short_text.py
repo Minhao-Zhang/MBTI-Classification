58087325188b85a917058275e9fda9cb0f08e1a1
Minhao Zhang <z.minhao.01+coding@gmail.com>
2024-07-23 14:41:51 +0800

    altered dataset

--- preprocessing/combine_short_text.py
+++ preprocessing/combine_short_text.py
@@ -4,16 +4,105 @@ DATA_PATH = "../data/"
 
 data = pd.read_csv(f'{DATA_PATH}reddit_post.csv')
 
+def split_2000(data: pd.DataFrame):
+    # find data that are longer than 2000 characters 
+    data_long = data[data['body'].str.len() > 2000]
+    # remove data that are longer than 2000 characters
+    data = data[data['body'].str.len() <= 2000]
+    # split the data that are longer than 2000 characters into multiple rows
+    new_rows = []
+    # find the index of the first space after 1000 characters
+    for index, row in data_long.iterrows():        
+        body = row['body']
+        first_space = body.find(' ', 800)
+        new_rows.append({
+            'author': row['author'],
+            'body': body[:first_space].strip(),
+            'mbti': row['mbti']
+        })
+        second_space = body.find(' ', first_space+800)
+        new_rows.append({
+            'author': row['author'],
+            'body': body[first_space:second_space].strip(),
+            'mbti': row['mbti']
+        })
+        new_rows.append({
+            'author': row['author'],
+            'body': body[second_space:].strip(),
+            'mbti': row['mbti']
+        })
+    temp_df = pd.DataFrame(new_rows)
+    print(temp_df.describe())
+    data = pd.concat([data, temp_df])
+    return data
+data = split_2000(data)
+
+def split_1400(data: pd.DataFrame):
+    # find data that are longer than 1600 characters 
+    data_long = data[data['body'].str.len() > 1400]
+    # remove data that are longer than 1600 characters
+    data = data[data['body'].str.len() <= 1400]
+    # split the data that are longer than 1600 characters into multiple rows
+    new_rows = []
+    # find the index of the first space after 800 characters
+    for index, row in data_long.iterrows():        
+        body = row['body']
+        sep = len(body) // 2
+        first_space = body.find(' ', sep)
+        new_rows.append({
+            'author': row['author'],
+            'body': body[:first_space].strip(),
+            'mbti': row['mbti']
+        })
+        new_rows.append({
+            'author': row['author'],
+            'body': body[first_space:].strip(),
+            'mbti': row['mbti']
+        })
+    temp_df = pd.DataFrame(new_rows)
+    print(temp_df.describe())
+    data = pd.concat([data, temp_df])
+    return data
+data = split_1400(data)
+
+def split_1000(data: pd.DataFrame):
+    # find data that are longer than 1000 characters 
+    data_long = data[data['body'].str.len() > 1000]
+    # remove data that are longer than 1000 characters
+    data = data[data['body'].str.len() <= 1000]
+    # split the data that are longer than 1000 characters into multiple rows
+    new_rows = []
+    # find the index of the first space after 500 characters
+    for index, row in data_long.iterrows():        
+        body = row['body']
+        # find the last space after 1000 characters
+        first_space = body.find(' ', 900)
+        new_rows.append({
+            'author': row['author'],
+            'body': body[:first_space].strip(),
+            'mbti': row['mbti']
+        })
+        new_rows.append({
+            'author': row['author'],
+            'body': body[first_space:].strip(),
+            'mbti': row['mbti']
+        })
+    temp_df = pd.DataFrame(new_rows)
+    print(temp_df.describe())
+    data = pd.concat([data, temp_df])
+    return data
+data = split_1000(data)
+
 # Function to concatenate body strings of each author
-def concatenate_bodies(group, min_length=500):
+def concatenate_bodies(group, min_length):
     concatenated = ''
     for body in group['body']:
         if len(body) >= min_length:
-            yield body
+            yield body.strip()
         else:
             if len(concatenated) > 0:
                 concatenated += ' '
-            concatenated += body
+            concatenated += body.strip()
             if len(concatenated) >= min_length:
                 yield concatenated
                 concatenated = ''
@@ -23,25 +112,39 @@ def concatenate_bodies(group, min_length=500):
 # List to collect the new rows
 new_rows = []
 
-n = 700
-
 # Group by 'author' and process each group
 for author, group in data.groupby('author'):
-    for concatenated_body in concatenate_bodies(group, n):
+    for concatenated_body in concatenate_bodies(group, 700):
         new_rows.append({
             'author': author,
             'body': concatenated_body,
             'mbti': group.iloc[0]['mbti']  # assuming 'mbti' is the same for all rows of the same author
         })
-        
 
 # Create a new DataFrame from the new rows
 new_df = pd.DataFrame(new_rows)
 
-# remove the rows with less than n char, print them 
-new_df = new_df[new_df['body'].str.len() >= n]
-# remove any value that are more than 90th percentile
-new_df = new_df[new_df['body'].str.len() <= new_df['body'].str.len().quantile(0.9)]
+def trim_body(data: pd.DataFrame, n: int):
+    data_long = data[data['body'].str.len() > n]
+    data = data[data['body'].str.len() < n]
+    new_rows = []
+    for index, row in data_long.iterrows():
+        body = row['body']
+        last_space = body.rfind(' ', 0, n)
+        new_rows.append({
+            'author': row['author'],
+            'body': body[:last_space].strip(),
+            'mbti': row['mbti']
+        })
+    temp_df = pd.DataFrame(new_rows)
+    data = pd.concat([data, temp_df])
+    return data
+new_df = trim_body(new_df, 1000)
+
+new_df = new_df[new_df['body'].str.len() >= 700]
+# plot the histogram of the length of body in new data bin=50
+
+print(new_df.describe())
 
 # shuffle the data  
 new_df = new_df.sample(frac=1).reset_index(drop=True)
